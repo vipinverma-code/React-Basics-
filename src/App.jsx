3675f565a599386781532d26f4904a6f008af7ce
vipinverma <vipinverma4224@gmail.com>
2026-01-28 22:39:13 +0530

    some changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer"
+function App(){
+    return(
+        <div>
+            <h1>Hello Everyone!</h1>
+            <Navbar/>
+            <Footer/>
+        </div>
+    );
+}
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+export const Footer = () => {
+  return (
+    <div>Contact Us: vipin@wehandle
+        <p>We try to support you</p>
+        <p>For connecting to our platform ,update and stay tunned </p>  
+
+    </div>
+  )
+}
+export default Footer;
